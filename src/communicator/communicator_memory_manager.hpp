de032a751813e83e4c1727386f33a1d027a21863
drfailer <drfailer.42@gmail.com>
2025-09-08 14:53:09 -0400

    wait for memory

--- src/communicator/communicator_memory_manager.hpp
+++ src/communicator/communicator_memory_manager.hpp
@@ -1,55 +1,103 @@
 #ifndef COMMUNICATOR_COMMUNICATOR_MEMORY_MANAGER
 #define COMMUNICATOR_COMMUNICATOR_MEMORY_MANAGER
+#include "../log.hpp"
+#include <condition_variable>
 #include <memory>
+#include <mutex>
 #include <tuple>
 #include <vector>
-#include "../log.hpp"
 
 namespace hh {
 
 namespace tool {
 
-template <typename... Types> struct CommunicatorMemoryManager {
-  std::tuple<std::shared_ptr<std::vector<std::shared_ptr<Types>>>...> pools = {};
+template <typename T> struct MemoryPool {
+  std::vector<std::shared_ptr<T>> memory;
+  std::mutex mutex;
+  std::condition_variable cv;
 
-  template <typename T> std::shared_ptr<std::vector<std::shared_ptr<T>>> &pool() {
-    return std::get<std::shared_ptr<std::vector<std::shared_ptr<T>>>>(pools);
+  std::shared_ptr<T> getMemory() {
+    std::unique_lock<std::mutex> poolLock(mutex);
+
+    if (memory.empty()) {
+      return nullptr;
+    }
+    auto data = memory.back();
+    memory.pop_back();
+    return data;
+  }
+
+  std::shared_ptr<T> getMemoryOrWait() {
+    std::unique_lock<std::mutex> poolLock(mutex);
+
+    if (memory.empty()) {
+      cv.wait(poolLock, [&]() { return !memory.empty(); });
+    }
+    auto data = memory.back();
+    memory.pop_back();
+    return data;
+  }
+
+  void returnMemory(std::shared_ptr<T> data) {
+    std::lock_guard<std::mutex> poolLock(mutex);
+    memory.push_back(data);
+    cv.notify_one();
+  }
+
+  void preallocate(size_t size, auto &&...args) {
+    std::lock_guard<std::mutex> poolLock(mutex);
+    memory.resize(size, nullptr);
+    for (auto &data : memory) {
+      data = std::make_shared<T>(std::forward<decltype(args)>(args)...);
+    }
+  }
+};
+
+template <typename... Types> struct CommunicatorMemoryManager {
+  std::tuple<std::shared_ptr<MemoryPool<Types>>...> pools = {};
+
+  template <typename T> std::shared_ptr<MemoryPool<T>> &pool() {
+    return std::get<std::shared_ptr<MemoryPool<T>>>(pools);
   }
 
   template <typename T> void preallocate(size_t size, auto &&...args) {
     auto &pool = this->template pool<T>();
 
     if (pool == nullptr) {
-      pool = std::make_shared<std::vector<std::shared_ptr<T>>>();
+      pool = std::make_shared<MemoryPool<T>>();
     }
-
-    pool->resize(size, nullptr);
-    for (size_t i = 0; i < size; ++i) {
-      pool->operator[](i) = std::make_shared<T>(std::forward<decltype(args)>(args)...);
-    }
+    pool->preallocate(size, std::forward<decltype(args)>(args)...);
   }
 
   template <typename T> std::shared_ptr<T> getMemory() {
     auto &pool = this->template pool<T>();
 
-    if (pool == nullptr || pool->empty()) {
+    if (pool == nullptr) {
       logh::error("memory pool emtpy.");
       return nullptr;
     }
-    auto ptr = pool->back();
-    pool->pop_back();
-    return ptr;
+    return pool->getMemory();
   }
 
-  template <typename T> void returnMemory(std::shared_ptr<T> elt) {
+  template <typename T> std::shared_ptr<T> getMemoryOrWait() {
+    auto &pool = this->template pool<T>();
+
+    if (pool == nullptr) {
+      logh::error("memory pool emtpy.");
+      return nullptr;
+    }
+    return pool->getMemoryOrWait();
+  }
+
+  template <typename T> void returnMemory(std::shared_ptr<T> data) {
     auto &pool = this->template pool<T>();
 
     // FIXME: there is now way to know if the input data belongs to the mm, therefore we add it anyway for now
     if (pool == nullptr) {
-      pool = std::make_shared<std::vector<std::shared_ptr<T>>>();
+      pool = std::make_shared<MemoryPool<T>>();
     }
 
-    pool->push_back(elt);
+    pool->returnMemory(data);
   }
 
   template <typename... SubsetTypes> std::shared_ptr<CommunicatorMemoryManager<SubsetTypes...>> convert() {
