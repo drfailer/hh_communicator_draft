d2584ef0bfb0506431fe5547810cf522305e287a
drfailer <drfailer.42@gmail.com>
2025-09-08 12:32:24 -0400

    rewrite memory manager

--- src/communicator/communicator_memory_manager.hpp
+++ src/communicator/communicator_memory_manager.hpp
@@ -3,44 +3,60 @@
 #include <memory>
 #include <tuple>
 #include <vector>
+#include "../log.hpp"
 
 namespace hh {
 
 namespace tool {
 
-template <typename... Types> class CommunicatorMemoryManager {
-public:
-  template <typename T> using pool_type_t = std::vector<std::shared_ptr<T>>;
+template <typename... Types> struct CommunicatorMemoryManager {
+  std::tuple<std::shared_ptr<std::vector<std::shared_ptr<Types>>>...> pools = {};
 
-public:
+  template <typename T> std::shared_ptr<std::vector<std::shared_ptr<T>>> &pool() {
+    return std::get<std::shared_ptr<std::vector<std::shared_ptr<T>>>>(pools);
+  }
+
   template <typename T> void preallocate(size_t size, auto &&...args) {
-    auto &pool = std::get<pool_type_t<T>>(pools_);
-    pool.resize(size);
+    auto &pool = this->template pool<T>();
 
-    for (auto &elt : pool) {
-      elt = std::make_shared<T>(std::forward<decltype(args)>(args)...);
+    if (pool == nullptr) {
+      pool = std::make_shared<std::vector<std::shared_ptr<T>>>();
+    }
+
+    pool->resize(size, nullptr);
+    for (size_t i = 0; i < size; ++i) {
+      pool->operator[](i) = std::make_shared<T>(std::forward<decltype(args)>(args)...);
     }
   }
 
   template <typename T> std::shared_ptr<T> getMemory() {
-    auto &pool = std::get<pool_type_t<T>>(pools_);
-    std::shared_ptr<T> elt = nullptr;
+    auto &pool = this->template pool<T>();
 
-    if (!pool.empty()) {
-        elt = pool.back();
-        pool.pop_back();
+    if (pool == nullptr || pool->empty()) {
+      logh::error("memory pool emtpy.");
+      return nullptr;
     }
-    return elt;
+    auto ptr = pool->back();
+    pool->pop_back();
+    return ptr;
   }
 
-  template <typename T>
-  void returnMemory(std::shared_ptr<T> &&elt) {
-    auto &pool = std::get<pool_type_t<T>>(pools_);
-    pool.emplace_back(std::move(elt));
+  template <typename T> void returnMemory(std::shared_ptr<T> elt) {
+    auto &pool = this->template pool<T>();
+
+    // FIXME: there is now way to know if the input data belongs to the mm, therefore we add it anyway for now
+    if (pool == nullptr) {
+      pool = std::make_shared<std::vector<std::shared_ptr<T>>>();
+    }
+
+    pool->push_back(elt);
   }
 
-private:
-  std::tuple<pool_type_t<Types>...> pools_;
+  template <typename... SubsetTypes> std::shared_ptr<CommunicatorMemoryManager<SubsetTypes...>> convert() {
+    auto mm = std::make_shared<CommunicatorMemoryManager<SubsetTypes...>>();
+    ((mm->template pool<SubsetTypes>() = this->template pool<SubsetTypes>()), ...);
+    return mm;
+  }
 };
 
 } // end namespace tool
