7d003f7b0f9a8b2e35ac7a3ce2f05c7d40562f87
drfailer <drfailer.42@gmail.com>
2025-09-12 15:19:04 -0400

    move lambda out of the loops

--- src/communicator/communicator_core_task.hpp
+++ src/communicator/communicator_core_task.hpp
@@ -12,14 +12,16 @@
 
 namespace hh {
 
-template <typename... Types> class CommunicatorTask;
+template <typename... Types>
+class CommunicatorTask;
 
 namespace core {
 
 template <typename... Types>
 using CommunicatorCoreTaskBase = GenericCoreTask<CommunicatorTask<Types...>, sizeof...(Types), Types..., Types...>;
 
-template <typename... Types> class CommunicatorCoreTask : public CommunicatorCoreTaskBase<Types...> {
+template <typename... Types>
+class CommunicatorCoreTask : public CommunicatorCoreTaskBase<Types...> {
 private:
   using TypesIds = typename CommunicatorTask<Types...>::TypesIds;
 
@@ -42,8 +44,8 @@ public:
     this->preRun();
 
     auto receivers = this->task()->comm()->receivers;
-    bool isReceiver =
-        std::find(receivers.begin(), receivers.end(), this->task()->comm()->comm->rank) != receivers.end();
+    bool isReceiver
+        = std::find(receivers.begin(), receivers.end(), this->task()->comm()->comm->rank) != receivers.end();
 
     if (isReceiver) {
       comm::commInfog(logh::IG::Core, "core", this->task()->comm(), "start receiver");
@@ -70,9 +72,9 @@ private:
   void taskLoop() {
     using namespace std::chrono_literals;
     std::chrono::time_point<std::chrono::system_clock> start, finish;
-    std::condition_variable sleepCondition;
-    std::vector<int> receivers;
-    bool canTerminate = false;
+    std::condition_variable                            sleepCondition;
+    std::vector<int>                                   receivers;
+    bool                                               canTerminate = false;
 
     comm::commInfog(logh::IG::CoreTaskLoop, "core task loop", this->task()->comm(),
                     "start task loop, canTerminate = ", canTerminate);
@@ -102,7 +104,15 @@ private:
 
   void sendDeamon() {
     using namespace std::chrono_literals;
+    auto returnData = [&]<typename T>(std::shared_ptr<T> data) {
+      if (mm_) {
+        mm_->returnMemory(std::move(data));
+      }
+    };
+
     while (!this->canTerminate()) {
+
+      //////////////////////////////////////////////////
       static size_t dbg_idx = 0;
       if (dbg_idx++ == 4000) {
         dbg_idx = 0;
@@ -111,34 +121,35 @@ private:
                    ", queue size = ", this->task()->comm()->queues.sendOps.size(),
                    ", hasNotifierConnected = ", this->hasNotifierConnected());
       }
-      comm::commProcessSendOpsQueue(this->task()->comm(), [&]<typename T>(std::shared_ptr<T> data) {
-        if (mm_) {
-          // TODO: if the data deos not come from this mm?
-          mm_->returnMemory(std::move(data));
-        }
-      });
+      //////////////////////////////////////////////////
+
+      comm::commProcessSendOpsQueue(this->task()->comm(), returnData);
       std::this_thread::sleep_for(4ms);
     }
     comm::commSendSignal(this->task()->comm(), this->task()->comm()->receivers, comm::CommSignal::Disconnect);
     comm::commInfog(logh::IG::SenderDisconnect, "sender disconnect", this->task()->comm());
-    comm::commProcessSendOpsQueue(
-        this->task()->comm(),
-        [&]<typename T>(std::shared_ptr<T> data) {
-          if (mm_) {
-            // TODO: if the data deos not come from this mm?
-            mm_->returnMemory(std::move(data));
-          }
-        },
-        true);
+    comm::commProcessSendOpsQueue(this->task()->comm(), returnData, true);
     comm::commInfog(logh::IG::SenderEnd, "sender end", this->task()->comm());
   }
 
   void recvDeamon() {
     using namespace std::chrono_literals;
     std::vector<bool> connections(this->task()->comm()->comm->nbProcesses, true);
-    int source = -1;
-    comm::CommSignal signal = comm::CommSignal::None;
-    comm::Header header;
+    int               source = -1;
+    comm::CommSignal  signal = comm::CommSignal::None;
+    comm::Header      header;
+    auto              createData = [&]<typename T>() {
+      if (!mm_) {
+        if constexpr (std::is_default_constructible_v<T>) {
+          return std::make_shared<T>();
+        } else {
+          throw std::runtime_error(
+                           "error: fail to create data in communicator task, provide a memory manager to solve this issue.");
+        }
+      }
+      return mm_->template getMemory<T>(false);
+    };
+    auto processData = [&]<typename T>(std::shared_ptr<T> data) { this->task()->addResult(data); };
 
     for (auto receiver : this->task()->comm()->receivers) {
       connections[receiver] = false;
@@ -148,9 +159,21 @@ private:
     //       timer when isConnected is false. After the timer, the thread leaves
     //       the loops and flushes the queue, however, this should be reported
     //       as an error in the dot file.
-    while (isConnected(connections) || !this->task()->comm()->queues.recvOps.empty() ||
-           !this->task()->comm()->queues.recvDataQueue.empty()) {
+    while (isConnected(connections) || !this->task()->comm()->queues.recvOps.empty()
+           || !this->task()->comm()->queues.recvDataQueue.empty()) {
       comm::commRecvSignal(this->task()->comm(), source, signal, header);
+
+      //////////////////////////////////////////////////
+      static size_t dbg_idx = 0;
+      if (dbg_idx++ == 4000) {
+        dbg_idx = 0;
+        logh::warn("reciever still running: channel = ", (int)this->task()->comm()->channel,
+                   ", rank = ", this->task()->comm()->comm->rank,
+                   ", data queue size = ", this->task()->comm()->queues.recvDataQueue.size(),
+                   ", ops queue size = ", this->task()->comm()->queues.recvOps.size(), ", connections = ", connections,
+                   ", hasNotifierConnected = ", this->hasNotifierConnected());
+      }
+      //////////////////////////////////////////////////
 
       if (!isConnected(connections)) {
         logh::error("non connected task: ops queue size = ", this->task()->comm()->queues.recvOps.size(),
@@ -169,19 +192,8 @@ private:
       case comm::CommSignal::Data:
         break;
       }
-      comm::commProcessRecvDataQueue(this->task()->comm(), [&]<typename T>() {
-        if (!mm_) {
-          if constexpr (std::is_default_constructible_v<T>) {
-            return std::make_shared<T>();
-          } else {
-            throw std::runtime_error(
-                "error: fail to create data in communicator task, provide a memory manager to solve this issue.");
-          }
-        }
-        return mm_->template getMemory<T>();
-      });
-      comm::commProcessRecvOpsQueue(this->task()->comm(),
-                                    [&]<typename T>(std::shared_ptr<T> data) { this->task()->addResult(data); });
+      comm::commProcessRecvDataQueue(this->task()->comm(), createData);
+      comm::commProcessRecvOpsQueue(this->task()->comm(), processData);
       std::this_thread::sleep_for(4ms);
     }
     comm::commInfog(logh::IG::ReceiverEnd, "receiver end", this->task()->comm());
@@ -189,8 +201,12 @@ private:
 
 public:
   [[nodiscard]] std::string extraPrintingInformation() const override {
-    std::string infos;
+    std::string                      infos;
     std::vector<comm::CommTaskStats> stats;
+
+    if (this->mm_) {
+      infos += mm_->extraPrintingInformation();
+    }
 
     if (!this->task()->comm()->comm->collectStats || this->task()->comm()->comm->nbProcesses == 1) {
       return infos;
@@ -202,20 +218,20 @@ public:
     if (this->task()->comm()->comm->rank == 0) {
       stats = comm::commGatherStats(this->task()->comm());
       std::map<comm::StorageId, comm::StorageInfo> storageStats;
-      size_t maxSendOpsSize = 0;
-      size_t maxRecvOpsSize = 0;
-      size_t maxRecvDataQueueSize = 0;
-      size_t maxSendStorageSize = 0;
-      size_t maxRecvStorageSize = 0;
-      auto transmissionStats = computeTransmissionStats(stats);
+      size_t                                       maxSendOpsSize = 0;
+      size_t                                       maxRecvOpsSize = 0;
+      size_t                                       maxRecvDataQueueSize = 0;
+      size_t                                       maxSendStorageSize = 0;
+      size_t                                       maxRecvStorageSize = 0;
+      auto                                         transmissionStats = computeTransmissionStats(stats);
 
-      infos.append("nbProcesses = " + std::to_string(nbProcesses) + "\n");
+      infos.append("nbProcesses = " + std::to_string(nbProcesses) + "\\l");
       std::string receiversStr = "[" + std::to_string(this->task()->comm()->receivers[0]);
       for (size_t i = 1; i < this->task()->comm()->receivers.size(); ++i) {
         receiversStr += ", " + std::to_string(this->task()->comm()->receivers[i]);
       }
       receiversStr += "]";
-      infos.append("receivers = " + receiversStr + "\n");
+      strAppend(infos, "receivers = " + receiversStr);
 
       for (auto const &stat : stats) {
         maxSendOpsSize = std::max(maxSendOpsSize, stat.maxSendOpsSize);
@@ -224,17 +240,17 @@ public:
         maxSendStorageSize = std::max(maxSendStorageSize, stat.maxSendStorageSize);
         maxRecvStorageSize = std::max(maxRecvStorageSize, stat.maxRecvStorageSize);
       }
-      infos.append("maxSendOpsSize = " + std::to_string(maxSendOpsSize) + "\n");
-      infos.append("maxRecvOpsSize = " + std::to_string(maxRecvOpsSize) + "\n");
-      infos.append("maxRecvDataQueueSize = " + std::to_string(maxRecvDataQueueSize) + "\n");
-      infos.append("maxSendStorageSize = " + std::to_string(maxSendStorageSize) + "\n");
-      infos.append("maxRecvStorageSize = " + std::to_string(maxRecvStorageSize) + "\n");
+      strAppend(infos, "maxSendOpsSize = " + std::to_string(maxSendOpsSize));
+      strAppend(infos, "maxRecvOpsSize = " + std::to_string(maxRecvOpsSize));
+      strAppend(infos, "maxRecvDataQueueSize = " + std::to_string(maxRecvDataQueueSize));
+      strAppend(infos, "maxSendStorageSize = " + std::to_string(maxSendStorageSize));
+      strAppend(infos, "maxRecvStorageSize = " + std::to_string(maxRecvStorageSize));
 
       for (comm::u8 typeId = 0; typeId < TypesIds().size; ++typeId) {
         if (!transmissionStats.contains(typeId)) {
           continue;
         }
-        strAppend(infos, "=== type :: ", (int)typeId, " ===");
+        infos.append("========== type :: " + std::to_string((int)typeId) + " ==========\n");
         auto transmissionDelays = transmissionStats.at(typeId).transmissionDelays;
         auto packingDelay = transmissionStats.at(typeId).packingDelay;
         auto unpackingDelay = transmissionStats.at(typeId).unpackingDelay;
@@ -242,17 +258,17 @@ public:
         strAppend(infos, "packing: ", packingDelay, "us, (count = ", packingDelay.size(), ")");
         strAppend(infos, "unpacking: ", unpackingDelay, "us, (count = ", unpackingDelay.size(), ")");
         strAppend(infos, "bandWdith: ", bandWidth, "MB/s");
-        infos.append("transmission: {\n");
+        infos.append("transmission: {\\l");
         for (size_t sender = 0; sender < nbProcesses; ++sender) {
           for (size_t receiver = 0; receiver < nbProcesses; ++receiver) {
             if (transmissionDelays[sender * nbProcesses + receiver].empty()) {
               continue;
             }
-            strAppend(infos, "[", sender, " -> ", receiver, "] = ", transmissionDelays[sender * nbProcesses + receiver],
-                      "us");
+            strAppend(infos, "    [", sender, " -> ", receiver,
+                      "] = ", transmissionDelays[sender * nbProcesses + receiver], "us");
           }
         }
-        infos.append("}\n");
+        strAppend(infos, "}");
       }
     } else {
       comm::commSendStats(this->task()->comm());
@@ -274,7 +290,7 @@ private:
           }
         }(),
         ...);
-    str.append(oss.str() + "\n");
+    str.append(oss.str() + "\\l");
   }
 
   static std::pair<double, double> computeAvg(std::vector<double> const &values) {
@@ -298,22 +314,22 @@ private:
   }
 
   struct TransmissionStat {
-    std::vector<double> packingDelay;
-    std::vector<double> unpackingDelay;
+    std::vector<double>              packingDelay;
+    std::vector<double>              unpackingDelay;
     std::vector<std::vector<double>> transmissionDelays;
-    std::vector<double> bandWdith;
+    std::vector<double>              bandWdith;
   };
 
   std::map<comm::u8, TransmissionStat> computeTransmissionStats(std::vector<comm::CommTaskStats> const &stats) const {
     std::map<comm::u8, TransmissionStat> transmissionStats;
-    size_t nbProcesses = this->task()->comm()->comm->nbProcesses;
+    size_t                               nbProcesses = this->task()->comm()->comm->nbProcesses;
 
     for (auto receiverRank : this->task()->comm()->receivers) {
       for (auto recvStorageStat : stats[receiverRank].storageStats) {
-        comm::StorageId storageId = recvStorageStat.first;
+        comm::StorageId   storageId = recvStorageStat.first;
         comm::StorageInfo recvInfos = recvStorageStat.second;
-        comm::u32 source = storageId.source;
-        comm::u8 typeId = recvInfos.typeId;
+        comm::u32         source = storageId.source;
+        comm::u8          typeId = recvInfos.typeId;
 
         if (stats[source].storageStats.contains(storageId)) {
           auto sendInfos = stats[source].storageStats.at(storageId);
@@ -329,11 +345,12 @@ private:
             });
           }
           auto delay = std::chrono::duration_cast<std::chrono::nanoseconds>(recvInfos.recvtp - sendInfos.sendtp);
-          transmissionStats.at(typeId).transmissionDelays[source * nbProcesses + receiverRank].push_back(delay.count() / 1'000'000'000.);
+          transmissionStats.at(typeId).transmissionDelays[source * nbProcesses + receiverRank].push_back(
+              delay.count() / 1'000'000'000.);
           transmissionStats.at(typeId).packingDelay.push_back(sendInfos.packingTime.count() / 1'000'000'000.);
           transmissionStats.at(typeId).unpackingDelay.push_back(recvInfos.unpackingTime.count() / 1'000'000'000.);
-          transmissionStats.at(typeId).bandWdith.push_back((sendInfos.dataSize / (1024. * 1024.)) /
-                                                           (delay.count() / 1'000'000'000.));
+          transmissionStats.at(typeId).bandWdith.push_back((sendInfos.dataSize / (1024. * 1024.))
+                                                           / (delay.count() / 1'000'000'000.));
         }
       }
     }
@@ -341,10 +358,12 @@ private:
   }
 
 public:
-  void setMemoryManager(std::shared_ptr<tool::CommunicatorMemoryManager<Types...>> mm) { this->mm_ = mm; }
+  void setMemoryManager(std::shared_ptr<tool::CommunicatorMemoryManager<Types...>> mm) {
+    this->mm_ = mm;
+  }
 
 private:
-  std::thread deamon_;
+  std::thread                                                deamon_;
   std::shared_ptr<tool::CommunicatorMemoryManager<Types...>> mm_;
 };
 
