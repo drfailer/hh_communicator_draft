90fd36259207559bb04fac3cdd512e47e91514eb
drfailer <drfailer.42@gmail.com>
2025-09-12 11:29:22 -0400

    memory pool stats

--- src/communicator/communicator_memory_manager.hpp
+++ src/communicator/communicator_memory_manager.hpp
@@ -15,10 +15,14 @@ template <typename T> struct MemoryPool {
   std::vector<std::shared_ptr<T>> memory;
   std::mutex mutex;
   std::condition_variable cv;
+  size_t preallocatedSize;
+  size_t nbGetMemory;
+  size_t nbReturnMemory;
 
   std::shared_ptr<T> getMemory() {
     std::lock_guard<std::mutex> poolLock(mutex);
 
+    ++nbGetMemory;
     if (memory.empty()) {
       return nullptr;
     }
@@ -30,8 +34,9 @@ template <typename T> struct MemoryPool {
   std::shared_ptr<T> getMemoryOrWait() {
     std::unique_lock<std::mutex> poolLock(mutex);
 
+    ++nbGetMemory;
     if (memory.empty()) {
-      logh::warn("waiting for memory pool");
+      logh::warn("waiting for memory pool: ", std::string(typeid(memory.data()).name()));
       cv.wait(poolLock, [&]() { return !memory.empty(); });
       logh::warn("end waiting");
     }
@@ -41,12 +46,25 @@ template <typename T> struct MemoryPool {
   }
 
   bool returnMemory(std::shared_ptr<T> &&data) {
-    if (data.use_count() > 1) {
-      // security so that we don't return any memory that is still referenced.
-      // The `returnMemory` function requires to move the pointer, therefore
-      // the count sould be 1 here.
-      return false;
+    if constexpr (requires { data->postProcess(); }) {
+        data->postProcess();
     }
+    if constexpr (requires { data->canBeRecycle(); }) {
+      if (!data->canBeRecycle()) {
+        return false;
+      }
+    } else {
+      if (data.use_count() > 1) {
+        // security so that we don't return any memory that is still referenced.
+        // The `returnMemory` function requires to move the pointer, therefore
+        // the count sould be 1 here.
+        return false;
+      }
+    }
+    if constexpr (requires { data->cleanMemory(); }) {
+      data->cleanMemory();
+    }
+    ++nbReturnMemory;
     std::lock_guard<std::mutex> poolLock(mutex);
     memory.push_back(data);
     cv.notify_all();
@@ -55,10 +73,19 @@ template <typename T> struct MemoryPool {
 
   void preallocate(size_t size, auto &&...args) {
     std::lock_guard<std::mutex> poolLock(mutex);
+    preallocatedSize = size;
     memory.resize(size, nullptr);
     for (auto &data : memory) {
       data = std::make_shared<T>(std::forward<decltype(args)>(args)...);
     }
+  }
+
+  std::string extraPrintingInformation() const {
+    // FIXME: GCC bug -> cannot use declval to create the object and print the type
+    return "MemoryPool[" + std::string(typeid(memory.data()).name()) + "]: { " +
+           "preallocatedSize = " + std::to_string(preallocatedSize) + ", nbGetMemory = " + std::to_string(nbGetMemory) +
+           ", nbReturnMemory = " + std::to_string(nbReturnMemory) + ", poolSize = " + std::to_string(memory.size()) +
+           " }";
   }
 };
 
@@ -66,6 +93,10 @@ template <typename... Types> struct CommunicatorMemoryManager {
   std::tuple<std::shared_ptr<MemoryPool<Types>>...> pools = {};
 
   template <typename T> std::shared_ptr<MemoryPool<T>> &pool() {
+    return std::get<std::shared_ptr<MemoryPool<T>>>(pools);
+  }
+
+  template <typename T> std::shared_ptr<MemoryPool<T>> pool() const {
     return std::get<std::shared_ptr<MemoryPool<T>>>(pools);
   }
 
@@ -113,6 +144,19 @@ template <typename... Types> struct CommunicatorMemoryManager {
     auto mm = std::make_shared<CommunicatorMemoryManager<SubsetTypes...>>();
     ((mm->template pool<SubsetTypes>() = this->template pool<SubsetTypes>()), ...);
     return mm;
+  }
+
+  std::string extraPrintingInformation() const {
+    std::string infos = "MemoryManager: {\\l";
+    (
+        [&] {
+          auto pool = this->template pool<Types>();
+          if (pool) {
+            infos.append("    " + pool->extraPrintingInformation() + "\\l");
+          }
+        }(),
+        ...);
+    return infos + "}\\l";
   }
 };
 
