c82657192a3940e107e5952f5f1d172dcc1ff9e4
drfailer <drfailer.42@gmail.com>
2025-09-17 10:52:04 -0400

    update ap

--- test/common/ap.h
+++ test/common/ap.h
@@ -5,46 +5,55 @@
 #include <functional>
 #include <map>
 #include <string>
+#include <vector>
 
 namespace ap {
 
+using AHFunction = std::function<void(std::string const &)>;
+
 struct ArgumentHandler {
-    std::function<void(std::string const &)> cb;
-    bool                                     expectsArg;
-    std::string                              doc;
+    AHFunction  cb;
+    bool        expectsArg;
+    std::string doc;
 };
 
 struct ArgumentParser {
     int                                    argc;
     char                                 **argv;
-    std::map<std::string, ArgumentHandler> args;
+    std::map<std::string, ArgumentHandler> dash_args;
+    std::vector<ArgumentHandler>           positional_args;
     std::string                            doc;
 };
 
 inline ArgumentParser argument_parser_create(int argc, char **argv,
                                              std::string const &doc = "") {
-    if (doc.empty()) {
-    }
     return ArgumentParser{
         .argc = argc,
         .argv = argv,
-        .args = {},
-        .doc = doc,
+        .dash_args = {},
+        .positional_args = {},
+        .doc = doc.empty() ? std::string(argv[0]) : doc,
     };
 }
 
-inline void add_arg(ArgumentParser *ap, std::string const &arg, auto cb,
+inline void add_arg(ArgumentParser *ap, std::string const &arg, AHFunction cb,
                     bool expects_value = true, std::string const &doc = "") {
-    ap->args.insert({arg, ArgumentHandler{cb, expects_value, doc}});
+    assert(arg[0] == '-');
+    ap->dash_args.insert({arg, ArgumentHandler{cb, expects_value, doc}});
 }
 
-#define AP_ADD_TYPE_ARG(type_, default_value_, arg_value_)                     \
+inline void add_arg(ArgumentParser *ap, AHFunction cb,
+                    std::string const &doc = "") {
+    ap->positional_args.push_back(ArgumentHandler{cb, false, doc});
+}
+
+#define AP_ADD_TYPE_DASH_ARG(type_, default_value_, arg_value_)                \
     inline void add_##type_##_arg(                                             \
         ArgumentParser *ap, std::string const &arg, type_ *variable,           \
         type_ default_value = default_value_, std::string const &doc = "") {   \
         assert(variable != NULL);                                              \
         *variable = default_value;                                             \
-        ap->args.insert(                                                       \
+        ap->dash_args.insert(                                                  \
             {arg, ArgumentHandler{                                             \
                       .cb =                                                    \
                           [variable](std::string const &arg_value) {           \
@@ -54,8 +63,8 @@ inline void add_arg(ArgumentParser *ap, std::string const &arg, auto cb,
                       .doc = doc,                                              \
                   }});                                                         \
     }
-AP_ADD_TYPE_ARG(int, 0, std::stoi(arg_value))
-AP_ADD_TYPE_ARG(size_t, 0, std::stoul(arg_value))
+AP_ADD_TYPE_DASH_ARG(int, 0, std::stoi(arg_value))
+AP_ADD_TYPE_DASH_ARG(size_t, 0, std::stoul(arg_value))
 
 inline void add_string_arg(ArgumentParser *ap, std::string const &arg,
                            std::string       *variable,
@@ -63,14 +72,14 @@ inline void add_string_arg(ArgumentParser *ap, std::string const &arg,
                            std::string const &doc = "") {
     assert(variable != NULL);
     *variable = default_value;
-    ap->args.insert({arg, ArgumentHandler{
-                              .cb =
-                                  [variable](std::string const &arg_value) {
-                                      *variable = arg_value;
-                                  },
-                              .expectsArg = true,
-                              .doc = doc,
-                          }});
+    ap->dash_args.insert(
+        {arg,
+         ArgumentHandler{
+             .cb = [variable](
+                       std::string const &arg_value) { *variable = arg_value; },
+             .expectsArg = true,
+             .doc = doc,
+         }});
 }
 
 inline void add_bool_arg(ArgumentParser *ap, std::string const &arg,
@@ -78,7 +87,7 @@ inline void add_bool_arg(ArgumentParser *ap, std::string const &arg,
                          std::string const &doc = "") {
     assert(variable != NULL);
     *variable = default_value;
-    ap->args.insert(
+    ap->dash_args.insert(
         {arg,
          ArgumentHandler{
              .cb = [variable, default_value](
@@ -90,7 +99,7 @@ inline void add_bool_arg(ArgumentParser *ap, std::string const &arg,
 
 inline void print_help(ArgumentParser *ap) {
     printf("%s:\n", ap->doc.c_str());
-    for (auto arg : ap->args) {
+    for (auto arg : ap->dash_args) {
         printf("\t%s :: %s\n", arg.first.c_str(), arg.second.doc.c_str());
     }
 }
@@ -113,58 +122,74 @@ inline ParsePrefixResult parse_prefix_(std::string const &str,
     return ParsePrefixResult{true, str.substr(prefix.size())};
 }
 
+inline int parse_dash_arg_(ArgumentParser *ap, std::string const &cur_arg,
+                           int *arg_idx) {
+    for (auto da : ap->dash_args) {
+        auto result = parse_prefix_(cur_arg, da.first);
+
+        if (result.ok) {
+            if (!da.second.expectsArg) {
+                da.second.cb("");
+                return 1;
+            }
+            if (result.rest[0] == '=' || result.rest.size() > 1) {
+                da.second.cb(result.rest.substr(1));
+            } else {
+                *arg_idx += 1;
+                if (*arg_idx >= ap->argc) {
+                    fprintf(stderr, "error: expected value for argument %s.\n",
+                            da.first.c_str());
+                    return -1;
+                }
+                da.second.cb(std::string(ap->argv[*arg_idx]));
+            }
+            return 1;
+        }
+    }
+    return 0;
+}
+
 enum class ArgumentParserStatus {
     Ok = 0,
     Help = 1,
-    UnknownArgumentError = -1,
-    InvalidValueError = -2,
+    Error = -1,
 };
-
 inline ArgumentParserStatus argument_parser_run(ArgumentParser *ap) {
-    int  arg_idx = 1;
-    bool is_arg_valid;
+    int    arg_idx = 1;
+    size_t positional_arg_idx = 0;
 
     while (arg_idx < ap->argc) {
-        std::string cur_arg(ap->argv[arg_idx]);
+        std::string arg(ap->argv[arg_idx]);
 
-        is_arg_valid = false;
-        for (auto arg : ap->args) {
-            auto result = parse_prefix_(cur_arg, arg.first);
-
-            if (result.ok) {
-                if (arg.second.expectsArg) {
-                    if (result.rest.starts_with('=')
-                        || result.rest.size() > 1) {
-                        arg.second.cb(result.rest.substr(1));
-                    } else {
-                        arg_idx += 1;
-                        if (arg_idx > ap->argc) {
-                            fprintf(stderr,
-                                    "error: expected value for argument %s\n",
-                                    arg.first.c_str());
-                            return ArgumentParserStatus::InvalidValueError;
-                        }
-                        std::string value(ap->argv[arg_idx]);
-                        arg.second.cb(value);
-                    }
-                } else {
-                    arg.second.cb("");
-                }
-                is_arg_valid = true;
-                break;
-            }
-        }
-        if (!is_arg_valid) {
-            if (cur_arg == "-h" || cur_arg == "--help") {
-                print_help(ap);
-                return ArgumentParserStatus::Help;
+        if (arg[0] != '-') {
+            if (positional_arg_idx < ap->positional_args.size()) {
+                ap->positional_args[positional_arg_idx++].cb(arg);
             } else {
-                fprintf(stderr, "error: expected value for argument %s\n",
-                        cur_arg.c_str());
-                return ArgumentParserStatus::UnknownArgumentError;
+                fprintf(stderr, "error: unexpected argument %s.\n",
+                        arg.c_str());
+                return ArgumentParserStatus::Error;
+            }
+        } else {
+            auto pdar = parse_dash_arg_(ap, arg, &arg_idx);
+
+            if (pdar < 0) {
+                return ArgumentParserStatus::Error;
+            } else if (pdar == 0) {
+                if (arg == "-h" || arg == "--help") {
+                    print_help(ap);
+                    return ArgumentParserStatus::Help;
+                } else {
+                    fprintf(stderr, "error: unknown argument %s\n",
+                            arg.c_str());
+                    return ArgumentParserStatus::Error;
+                }
             }
         }
         arg_idx += 1;
+    }
+    if (positional_arg_idx < ap->positional_args.size()) {
+        fprintf(stderr, "error: not all positional arguments were found.\n");
+        return ArgumentParserStatus::Error;
     }
     return ArgumentParserStatus::Ok;
 }
