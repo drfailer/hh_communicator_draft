c0e6b6a9211ed9ce13ac8a142f76827b7d874b46
drfailer <drfailer.42@gmail.com>
2026-02-06 15:04:39 -0500

    document the protocol file

--- src/communicator/protocol.hpp
+++ src/communicator/protocol.hpp
@@ -3,43 +3,70 @@
 #include "hedgehog/src/tools/meta_functions.h"
 #include <memory>
 
+/// @brief Hedgehog namespace
 namespace hh {
-
+/// @brief Communicator namespace
 namespace comm {
 
+/******************************************************************************/
+/*                                   Buffer                                   */
+/******************************************************************************/
+
+/// @brief Buffer structure.
 struct Buffer {
-  char  *mem;
-  size_t len;
+  char  *mem; ///< Pointer to the data.
+  size_t len; ///< Length of the data.
 };
 
-// Signal //////////////////////////////////////////////////////////////////////
+/******************************************************************************/
+/*                                   Signal                                   */
+/******************************************************************************/
 
+/// @brief Model the signal used in the requests.
 enum class Signal : std::uint8_t {
-  None,
-  Data,
-  Disconnect,
+  None,       ///< unknown signal
+  Data,       ///< the request contain some data.
+  Disconnect, ///< the request contains a disconnection signal.
 };
 
-// Header //////////////////////////////////////////////////////////////////////
+/******************************************************************************/
+/*                                   Header                                   */
+/******************************************************************************/
 
+// The following aliases are used to determin the types of the header
+// components. They also make the code more readable.
+
+/// @brief Channel id type.
 using channel_t = std::uint64_t;
+/// @brief Rank type.
 using rank_t = std::uint64_t;
+/// @brief Signal type.
 using signal_t = std::uint64_t;
+/// @brief Type of the type id.
 using type_id_t = std::uint64_t;
+/// @brief Type of the package id.
 using package_id_t = std::uint64_t;
+/// @brief Type of the buffer id.
 using buffer_id_t = std::uint64_t;
 
+/// @brief Header type used in the requests.
 struct Header {
-  channel_t    channel;
-  rank_t       source;
-  signal_t     signal;
-  type_id_t    typeId;
-  package_id_t packageId;
-  buffer_id_t  bufferId;
+  channel_t    channel;   ///< Channel id (which communicator task).
+  rank_t       source;    ///< Source that sent the requests.
+  signal_t     signal;    ///< Signal contained in the request.
+  type_id_t    typeId;    ///< Type id of the data contained in the request (if data).
+  package_id_t packageId; ///< Package id of the package stored in the request (used to match a buffer with a package).
+  buffer_id_t  bufferId;  ///< Buffer id of the data.
 
-  Header() = default;
-  Header(rank_t source, signal_t signal, type_id_t typeId, channel_t channel,
-         package_id_t packageId, buffer_id_t bufferId)
+  /// @brief constructor from all the elements.
+  /// @param source    Value of the source.
+  /// @param signal    Value of the signal.
+  /// @param typeId    Value of the typeId.
+  /// @param channel   Value of the channel.
+  /// @param packageId Value of the packageId.
+  /// @param bufferId  Value of the bufferId.
+  Header(rank_t source = 0, signal_t signal = 0, type_id_t typeId = 0, channel_t channel = 0,
+         package_id_t packageId = 0, buffer_id_t bufferId = 0)
       : channel(channel),
         source(source),
         signal(signal),
@@ -47,6 +74,7 @@ struct Header {
         packageId(packageId),
         bufferId(bufferId) {}
 
+  /// @brief enum that model the fileds of the header.
   enum Fields {
     CHANNEL = 0,
     SOURCE = 1,
@@ -56,11 +84,15 @@ struct Header {
     BUFFER_ID = 5,
   };
 
+  /// @brief Structure that can be used to encode/decode a header into a tag.
   struct FieldInfo {
-    size_t        offset;
-    std::uint64_t mask;
+    size_t        offset; ///< Offset of the field.
+    std::uint64_t mask;   ///< Mask to get the filed value.
   };
 
+  /// @brief Allow to compare headers.
+  /// @param other Other header to compare with.
+  /// @return true if `this` is inferior to `other`.
   bool operator<(Header const &other) const {
     std::uint64_t thisVals[]
         = {this->source, this->signal, this->typeId, this->channel, this->packageId, this->bufferId};
