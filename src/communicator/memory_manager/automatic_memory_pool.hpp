accc9e31d94638cfd31c67cd14ec9185647cdb63
drfailer <drfailer.42@gmail.com>
2026-04-09 15:46:50 -0400

    documentation

--- src/communicator/memory_manager/automatic_memory_pool.hpp
+++ src/communicator/memory_manager/automatic_memory_pool.hpp
@@ -18,23 +18,45 @@ namespace comm {
 /// @brief Tools namespace
 namespace tool {
 
+/******************************************************************************/
+/*                       SingleTypeAutomaticMemoryPool                        */
+/******************************************************************************/
+
+/// @brief Automatic memory pool that uses the shared pointer destructor to
+///        return memory.
+/// @tparam T Type managed by the pool.
 template <typename T>
 class SingleTypeAutomaticMemoryPool
     : public std::enable_shared_from_this<SingleTypeAutomaticMemoryPool<T>>,
       public SingleTypeMemoryManager<T> {
 public:
+  /// @brief Since it inherits from enable_shared_from_this, the constructor is
+  ///        private and we use a static create method that returns a shared pointer.
+  /// @return Shared pointer to an instance of the pool.
   static std::shared_ptr<SingleTypeAutomaticMemoryPool<T>> create() {
     return std::shared_ptr<SingleTypeAutomaticMemoryPool<T>>(new SingleTypeAutomaticMemoryPool<T>());
   }
 
+  /// @brief Deleted copy constructor.
   SingleTypeAutomaticMemoryPool(const SingleTypeAutomaticMemoryPool &) = delete;
+
+  /// @brief Deleted copy operator.
   SingleTypeAutomaticMemoryPool &operator=(const SingleTypeAutomaticMemoryPool &) = delete;
+
+  /// @brief Deleted move constructor.
   SingleTypeAutomaticMemoryPool(SingleTypeAutomaticMemoryPool &&) = delete;
+
+  /// @brief Deleted move operator.
   SingleTypeAutomaticMemoryPool &operator=(SingleTypeAutomaticMemoryPool &&) = delete;
 
+public:
+  /// @brief Resource deleter used the shared_ptr to return the memory when the
+  ///        use count is 0.
   struct ResourceDeleter {
-    std::weak_ptr<SingleTypeAutomaticMemoryPool> weakPool_;
+    std::weak_ptr<SingleTypeAutomaticMemoryPool> weakPool_; ///< Pointer to the pool.
 
+    /// @brief Return the given pointer to the automatic pool.
+    /// @param ptr Deleted pointer
     void operator()(T *ptr) const {
       if (auto pool = weakPool_.lock()) {
         pool->release_ptr(ptr);
@@ -44,6 +66,35 @@ public:
     }
   };
 
+  /// @brief Release method use by shared_ptr.
+  /// @param data Pointer to release.
+  void release_ptr(T *data) {
+    if (data == nullptr)
+      return;
+
+    const auto lg = std::lock_guard(mutex_);
+    if constexpr (requires { data->postProcess(); }) {
+      data->postProcess();
+    }
+    if constexpr (requires { data->canBeRecycled(); }) {
+      if (!data->canBeRecycled()) {
+        return;
+      }
+    }
+    if constexpr (requires { data->cleanMemory(); }) {
+      data->cleanMemory();
+    }
+
+#ifdef HH_COMM_DEBUG
+    alloc_locations_.erase((uintptr_t)data);
+#endif
+    memory_.emplace_back(std::unique_ptr<T>(data));
+    cv_.notify_all();
+  }
+
+  /// @brief Implementation of the `allocate` method for the SingleTypeMemoryManager
+  /// @param mode Allocation mode.
+  /// @param loc  Caller location (used for debuggin, see report_uses method).
   std::shared_ptr<T> allocate(const MemoryManagerAllocateMode mode = MemoryManagerAllocateMode::Fail,
                               [[maybe_unused]]std::source_location loc = std::source_location::current()) override {
     auto lock = std::unique_lock(mutex_);
@@ -76,31 +127,10 @@ public:
     return std::shared_ptr<T>(raw, ResourceDeleter{this->shared_from_this()});
   }
 
+  /// @brief Implementation of the `release` method for the
+  ///        SingleTypeMemoryManager (does nothing because we use the shared_ptr
+  ///        destructor to release)
   void release(std::shared_ptr<T> &&, std::source_location = std::source_location::current()) override { /* do nothing */ }
-
-  void release_ptr(T *data) {
-    if (data == nullptr)
-      return;
-
-    const auto lg = std::lock_guard(mutex_);
-    if constexpr (requires { data->postProcess(); }) {
-      data->postProcess();
-    }
-    if constexpr (requires { data->canBeRecycled(); }) {
-      if (!data->canBeRecycled()) {
-        return;
-      }
-    }
-    if constexpr (requires { data->cleanMemory(); }) {
-      data->cleanMemory();
-    }
-
-#ifdef HH_COMM_DEBUG
-    alloc_locations_.erase((uintptr_t)data);
-#endif
-    memory_.emplace_back(std::unique_ptr<T>(data));
-    cv_.notify_all();
-  }
 
   std::string extraPrintingInformation() const override {
     static std::string typeStr = hh::tool::typeToStr<T>();
@@ -109,6 +139,9 @@ public:
     return oss.str();
   }
 
+  /// @brief Preallocates elements.
+  /// @param capacity Number of elements to preallocate.
+  /// @param args     Arguments to pass to T's constructor.
   template <typename... Args>
   void fill(const size_t capacity, Args &&...args) {
     const auto lg = std::lock_guard(mutex_);
@@ -117,11 +150,15 @@ public:
     }
   }
 
+  /// @brief Return the number of elements in the pool.
+  /// @return Number of elements in the pool.
   [[nodiscard]] auto size() {
     const auto lg = std::lock_guard(mutex_);
     return memory_.size();
   }
 
+  /// @brief Report all the pointers that were not returned to the pool, and
+  ///        show the allocation location.
   void report_uses() {
 #ifdef HH_COMM_DEBUG
     const auto lg = std::lock_guard(mutex_);
@@ -133,23 +170,31 @@ public:
   }
 
 private:
-  explicit SingleTypeAutomaticMemoryPool() = default; // Use create() for shared_ptr management
+  /// @brief Private default constructor (Use create() for shared_ptr management)
+  explicit SingleTypeAutomaticMemoryPool() = default;
 
 private:
-  std::vector<std::unique_ptr<T>> memory_ = {};
-  std::mutex                      mutex_ = {};
-  std::condition_variable         cv_ = {};
+  std::vector<std::unique_ptr<T>> memory_ = {}; ///< memory of the pool.
+  std::mutex                      mutex_ = {};  ///< mutex for thread safety.
+  std::condition_variable         cv_ = {};     ///< condition variable used for the wait allocation mode.
 #ifdef HH_COMM_DEBUG
-  std::map<uintptr_t, std::source_location> alloc_locations_;
+  std::map<uintptr_t, std::source_location> alloc_locations_; ///< debug map that tracks the used pointers.
 #endif
 };
 
+/******************************************************************************/
+/*                            AutomaticMemoryPool                             */
+/******************************************************************************/
+
+/// @brief Automatic memory pool for multiple types.
+/// @tparam Types Types managed by the pool.
 template <typename... Types>
 class AutomaticMemoryPool {
 private:
-  std::tuple<std::shared_ptr<SingleTypeAutomaticMemoryPool<Types>>...> pools_;
+  std::tuple<std::shared_ptr<SingleTypeAutomaticMemoryPool<Types>>...> pools_; ///< single type memory pools.
 
 public:
+  /// @brief Default constructor of the pool.
   explicit AutomaticMemoryPool()
       : pools_(SingleTypeAutomaticMemoryPool<Types>::create()...) {};
 
@@ -180,11 +225,15 @@ public:
     std::get<std::shared_ptr<SingleTypeAutomaticMemoryPool<T>>>(this->pools_)->fill(capacity, args...);
   }
 
+  /// @brief Returns a pointer to a SingleTypeMemoryManager for the type T.
+  /// @tparam T Managed type.
+  /// @return Pointer to a SingleTypeMemoryManager.
   template <typename T>
   SingleTypeMemoryManager<T> *getMemoryManager() {
       return std::get<std::shared_ptr<SingleTypeAutomaticMemoryPool<T>>>(this->pools_).get();
   }
 
+  /// @brief Report the used pointers for all the managed types.
   void report_uses() {
 #ifdef HH_COMM_DEBUG
     (std::get<std::shared_ptr<SingleTypeAutomaticMemoryPool<Types>>>(this->pools_)->report_uses(), ...);
@@ -210,17 +259,5 @@ public:
 } // end namespace comm
 
 } // end namespace hh
-
-// int main() {
-//     const auto pool = SingleTypeAutomaticMemoryPool<Resource>::create();
-//     pool->fill(4, 8.16, 32);
-//
-//     {
-//         auto res = pool->allocate();
-//         printf("[Acquired %f %d]\n", res->val1_, res->val2_);
-//     }
-//
-//     return 0;
-// }
 
 #endif
